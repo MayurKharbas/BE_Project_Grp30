4b9011b8abd04fe921da3118e2e43372ba8b4ab7
MayurKharbas <211mayurskharbas@gmail.com>
2019-05-30 11:48:03 +0530

    Update

--- Arima_plot.py
+++ Arima_plot.py
@@ -1,43 +1,34 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[27]:
-
-
 import pandas as pd
 import numpy as np
-
 from sklearn.metrics import mean_squared_error
-
 import matplotlib.pyplot as plt
 
 
 train = pd.read_csv('train.csv')
-train.head()
-
 test = pd.read_csv('test.csv')
-test.head()
 
 colnames = ['activity_hour', 'total_activity']
-forecast = pd.read_csv('forecast1.csv', names=colnames, header=None)
-forecast.head()
+forecast = pd.read_csv('forecast.csv', names=colnames, header=None)
 
+# Generate the plot and save as png
 ax = train.plot(x='activity_hour', y='total_activity', label='train')
 test.plot(ax=ax, x='activity_hour', y='total_activity', label='test')
 forecast.plot(ax=ax, x='activity_hour', y='total_activity', label='model')
-plt.figure(figsize=(50, 30))
-plt.show()
+plt.savefig('plot.png')
 
+# Uncomment below line to show plot
+# plt.show()
 
-mse = abs((forecast.total_activity - test.total_activity))/test.total_activity
-mean_error = np.mean(mse)
-print(mean_error)
-
-below10 = mse[mse<0.].count()
-all = mse.count()
-
-print(below10, " : ",  all)
-print(mse)
+# mse = abs((forecast.total_activity - test.total_activity))/test.total_activity
+# mean_error = np.mean(mse)
+# print(mean_error)
+# below10 = mse[mse<0.].count()
+# all = mse.count()
+# print(below10, " : ",  all)
+# print(mse)
 
 
 
--- Main.py
+++ Main.py
@@ -6,12 +6,11 @@ import pandas as pd
 import itertools
 from itertools import product
 from sklearn.metrics import mean_squared_error
-
-import warnings
-warnings.filterwarnings('ignore')
-
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.tsa.seasonal import seasonal_decompose
+import warnings
+warnings.filterwarnings('ignore')
 
 
 def Process(grid_id):
@@ -36,7 +35,6 @@ def Process(grid_id):
 
 
 	# # Split dataset into train and test
-
 	train = df_grid[:125]
 	test = df_grid[125:]
 
@@ -45,23 +43,15 @@ def Process(grid_id):
 	test = test.set_index('activity_hour')
 
 
-	# # Fit Arima model
+	# Fit Arima model
 
-
-	'''
-	ARIMA model
-
-	parameters_list - list with (p, q, P, Q)
-	    p - associated with the auto-regressive aspect of the model
-	    d - integration order in ARIMA model (effects the amount of differencing to apply to a time series)
-	    D - seasonal integration order 
-	    
-	'''
-
-	p = d = q = range(0, 2)
-	pdq = list(itertools.product(p, d, q))
-	seasonal_pdq = [(x[0], x[1], x[2], 24) for x in pdq]
-
+	"""
+	parameters_list - (p, q, P, Q) tuples
+	p - associated with the auto-regressive aspect of the model
+	d - integration order in ARIMA model (effects the amount of differencing to apply to a time series)
+	D - seasonal integration order 
+	s - length of season
+	"""
 
 	# AIC Scores
 	# Akaike information criterion (AIC) (Akaike, 1974) 
@@ -69,31 +59,8 @@ def Process(grid_id):
 	# to estimate the likelihood of a model to predict/estimate the future values. 
 	# A good model is the one that has minimum AIC among all the other models.
 
-	for param in pdq:
-		for param_seasonal in seasonal_pdq:
-			try:
-				mod = sm.tsa.statespace.SARIMAX(train,order=param,
-								seasonal_order=param_seasonal,
-								enforce_stationarity=False,
-								enforce_invertibility=False)
-
-				results = mod.fit()
-
-				print('ARIMA{}x{}24 - AIC:{}'.format(param, param_seasonal, results.aic))
-
-			except:
-				continue
-
-
-	mod = sm.tsa.statespace.SARIMAX(train,
-		                        order=(1, 1, 1),
-		                        seasonal_order=(0, 1, 1, 24),
-		                        enforce_stationarity=False,
-		                        enforce_invertibility=False)
-	results = mod.fit()
-
+	
 	# setting initial values and some bounds for them
-	#ps = qs = range(2, 5)
 	ps = qs = Ps = Qs = range(0, 2)
 	d = 1
 	D = 1
@@ -102,87 +69,56 @@ def Process(grid_id):
 	# creating list with all the possible combinations of parameters
 	parameters = product(ps, qs, Ps, Qs)
 	parameters_list = list(parameters)
-	len(parameters_list)
 
+	best_aic = float("inf")
+	best_param = parameters_list[0]
 
-
-	'''
-	def optimizeSARIMA(parameters_list, d, D, s):
-	    """
-		Return df with parameters and corresponding AIC
-		
-		parameters_list - (p, q, P, Q) tuples
-		d - integration order in ARIMA model
-		D - seasonal integration order 
-		s - length of season
-	    """
-	    
-	    results = []
-	    best_aic = float("inf")
-
-	    for param in parameters_list:
-		# we need try-except because on some combinations model fails to converge
+	for param in parameters_list:
+		# since some combinations model fails to converge
 		try:
 		    model=sm.tsa.statespace.SARIMAX(train, order=(param[0], d, param[1]), 
-		                                    seasonal_order=(param[2], D, param[3], s)).fit(disp=-1)
+					            seasonal_order=(param[2], D, param[3], s)).fit(disp=-1)
 		except:
 		    continue
 		aic = model.aic
 		# saving best model, AIC and parameters
 		if aic < best_aic:
-		    best_model = model
 		    best_aic = aic
 		    best_param = param
-		results.append([param, model.aic])
-
-	    result_table = pd.DataFrame(results)
-	    result_table.columns = ['parameters', 'aic']
-	    # sorting in ascending order, the lower AIC is - the better
-	    result_table = result_table.sort_values(by='aic', ascending=True).reset_index(drop=True)
-	    
-	    return result_table
-	    
-	result_table = optimizeSARIMA(parameters_list, d, D, s)
-	print(result_table)'''
-
 
 
 	best_model = sm.tsa.statespace.SARIMAX(train,
-		                        order=(1, 1, 1),
-		                        seasonal_order=(0, 1, 1, 24),
+		                        order=(best_param[0], d, best_param[1]),
+		                        seasonal_order=(best_param[2], D, best_param[3], s),
 		                        enforce_stationarity=False,
 		                        enforce_invertibility=False).fit()
 
 
 	#Predict list
-
 	n = 41
 	data = train.copy()
 	data['arima_model'] = best_model.fittedvalues
-
 	forecast = pd.DataFrame(best_model.predict(start=data.shape[0], end=data.shape[0]+n))
 	
-	##forecast.to_csv('forecast1.csv', index='activity_hour', encoding='utf-8',header=False)
-	#forecast.to_csv('forecast1.csv', encoding='utf-8',header=False)
-	#colnames = ['activity_hour', 'total_activity']
-	#forecast = pd.read_csv('forecast1.csv', names=colnames, header=None)    
-	#forecast = forecast.set_index('activity_hour')
+	# Write the files
+	train.to_csv('train.csv', index='activity_hour', encoding='utf-8')
+	test.to_csv('test.csv', index='activity_hour', encoding='utf-8')  
+	forecast.to_csv('forecast.csv', encoding='utf-8',header=False)
 
-	#train.to_csv('train.csv', index='activity_hour', encoding='utf-8')
-	#test.to_csv('test.csv', index='activity_hour', encoding='utf-8')  
+	# Read the csv files as dataframe
+	colnames = ['activity_hour', 'total_activity']
+	forecast = pd.read_csv('forecast.csv', names=colnames, header=None)
+	train = pd.read_csv('train.csv')
+	test = pd.read_csv('test.csv')
 
-	final_lists = [train, test, forecast]
+	# Generate the plot and save as png
+	ax = train.plot(x='activity_hour', y='total_activity', label='train')
+	test.plot(ax=ax, x='activity_hour', y='total_activity', label='test')
+	forecast.plot(ax=ax, x='activity_hour', y='total_activity', label='model')
+	plt.savefig('{}.png'.format(grid_id))
+	# plt.savefig('plot.png')
 
-	return final_lists
+	# final_lists = [train, test, forecast]
+	# return final_lists
 
-if __name__ == "__main__":
-	'''
-	grid = 325
-	out = Process(grid)
-	
-	train1 = out[0]
-	test1 = out[1]
-	forecast1 = out[2]
-	
-	print(train1.head())
-	'''
+Process(147)
